0476ec3d00bfcb05a416387954b6261a4857c142
Krishna Jindal <88274406+kj-1809@users.noreply.github.com>
2024-01-02 01:26:47 +0530

    add : harpoon

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,41 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-						local builtin = require("telescope.builtin")
-						vim.keymap.set('n' , '<leader>ff' , builtin.find_files , {desc = "Find Files"});
-						vim.keymap.set('n' , '<leader>fd' , builtin.live_grep , {desc = "Live grep"});
-		end
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>fd", builtin.live_grep, { desc = "Live grep" })
+			vim.keymap.set("n" , "<leader>ff" , builtin.find_files , { desc = "Find Files"})
+			vim.keymap.set(
+				"n",
+				"<leader>fh",
+				"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git'}})<cr>",
+				{desc = "Advanced Find Files"}
+			)
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						'^Music/',
+						'^Library/',
+						'^Movies/',
+						'node_modules/'
+					}
+				}
+			})
+		end,
 	},
 	{
-		  'nvim-telescope/telescope-ui-select.nvim',
-			config = function()
-				require("telescope").setup {
-					extensions = {
-							["ui-select"] = {
-								require("telescope.themes").get_dropdown {
-								}
-							}
-						}
-					}
-				require("telescope").load_extension("ui-select")
-			end
-	}
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,7 +4,22 @@ vim.opt.shiftwidth = 2
 vim.keymap.set('i' , 'jk' , '<Esc>')
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.scrolloff = 8
+-- vim.opt.wrap = false
 
+-- move selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("i" , "<C-j>" , "<C-n>")
+
+-- highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
